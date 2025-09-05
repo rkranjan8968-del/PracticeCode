bd95f560900941f5128efdfc2e9ff4acf1e79dde
rkranjan8968-del <https://github.com/rkranjan8968-del/PracticeCode.git>
2025-09-05 16:10:43 +0530

    add another function

--- MaxMinList.py
+++ MaxMinList.py
@@ -14,6 +14,7 @@ print(find_max_min([3,4,9,0,1,99]))     # (99,0)
     
     
 
+
   
 # output using f-strings or print() with text.    
     
@@ -34,3 +35,10 @@ max_v,min_v = find_max_min(number)
 
 print(f"Max value is {max_v}")
 print(f"Min value is {min_v}")
+
+
+
+def find_max_min(list):
+    return max(list), min(list)
+
+print(find_max_min([29,89,69,79]))
