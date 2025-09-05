f45d301159d386dff6255a5e4ab04325c07bd745
rkranjan8968-del <https://github.com/rkranjan8968-del/PracticeCode.git>
2025-09-05 14:55:13 +0530

    initial commit

---
+++ MaxMinList.py
@@ -0,0 +1,36 @@
+def find_max_min(lst):
+    max_v = lst[0]        # assume first element is max
+    min_v = lst[0]        # assume first element is min
+    
+    for n in lst:         # go through each number
+        if n > max_v:      # check for bigger number
+            max_v = n
+        if n < min_v:      # check for smaller number
+            min_v = n
+    
+    return max_v,min_v      # return both
+
+print(find_max_min([3,4,9,0,1,99]))     # (99,0)
+    
+    
+
+  
+# output using f-strings or print() with text.    
+    
+def find_max_min(lst):
+    max_v = lst[4]        # assume first element is max
+    min_v = lst[4]        # assume first element is min
+    
+    for n in lst:         # go through each number
+        if n > max_v:      # check for bigger number
+            max_v = n
+        if n < min_v:      # check for smaller number
+            min_v = n
+    
+    return max_v,min_v   
+
+number = [89,34,2,3,77,599]
+max_v,min_v = find_max_min(number)
+
+print(f"Max value is {max_v}")
+print(f"Min value is {min_v}")
